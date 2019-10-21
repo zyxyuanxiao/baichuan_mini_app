516b2ef763a07878cd88aa523e24ec5733b7412f
weiqianlai <44868979+weiqianlai@users.noreply.github.com>
2019-10-21 23:41:00 +0800

    2019-10-21
    完善绑定手机号

--- main-app/pages/bind/bind.js
+++ main-app/pages/bind/bind.js
@@ -9,7 +9,7 @@ Page({
     tel: "",
     code: "",
     isValidTel: !1,
-    codename: '获取验证码',
+    codename: '获取验证码'
   },
 
   /**
@@ -28,7 +28,6 @@ Page({
     var that = this;
     var phone = that.data.phone;
     var openid = wx.getStorageSync("user_id");
-    console.log("aa", openid)
     if (!(/^(14[0-9]|13[0-9]|15[0-9]|17[0-9]|18[0-9])\d{8}$$/).test(phone)) {
       wx.showToast({
         title: '请输入正确的手机号',
@@ -41,9 +40,8 @@ Page({
         method: 'GET',
         data: {
           phone: phone,
-          openid:openid,
-          order :1,
-          code:""
+          openid: openid,
+          order: 1
         },
         header: {
           'Content-Type': 'application/x-www-form-urlencoded'
@@ -51,10 +49,7 @@ Page({
         method: 'GET',
         dataType: 'json',
         success: function(res) {
-          console.log("后台返回的:",res)
-          that.setData({
-            iscode: res.data.code
-          })
+          console.log("发送号码",res)
           var num = 61;
           var timer = setInterval(function() {
             num--;
@@ -67,7 +62,8 @@ Page({
 
             } else {
               that.setData({
-                codename: num + "s"
+                codename: num + "s",
+                disabled: true
               })
             }
           }, 1000)
@@ -75,8 +71,45 @@ Page({
       })
     }
   },
-  submit:function(){
-
+  handleCode: function(e) {
+    this.setData({
+      code: e.detail.value
+    })
+  },
+  submit: function() {
+    var that = this;
+    var openid = wx.getStorageSync("user_id");
+    var phone = that.data.phone;
+    var code = that.data.code;
+    wx.request({
+      url: app.API + 'bindPhone',
+      method: 'GET',
+      data: {
+        phone: phone,
+        openid: openid,
+        order: 0,
+        code: code
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'GET',
+      dataType: 'json',
+      success: function(res) {
+        console.log("huhuhu",res);
+        if (res.data.code == 0) {
+          wx.redirectTo({
+            url: '../products/products',
+          })
+        } else{
+          wx.showToast({
+            title: '验证码错误，请重新输入',
+            icon: 'none',
+            duration: 1000
+          })
+        }
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
